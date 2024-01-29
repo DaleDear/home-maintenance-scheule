5d1ba7dfb46a9625c0efa84227e3fe438e4729d6
Dale Dear <kristendaledear@gmail.com>
2024-01-28 21:31:35 -0600

    inspection list with buttons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { getAllInspections } from './services/inspectionService';
 
 export const App = () => {
   const [allInspections, setAllInspections] = useState([])
+  const [showInteriorOnly, setShowInteriorOnly] = useState(false)
+  const [filteredInspections, setFilteredInspections] = useState([])
+
 
   useEffect(() => {
    getAllInspections().then((inspectionsArray) => {
@@ -13,15 +16,29 @@ export const App = () => {
   })
 }, [])
 
+  useEffect(() => {
+    if (showInteriorOnly) {
+      const interiorInspections = allInspections.filter(inspection => inspection.interior === true)
+      setFilteredInspections(interiorInspections)
+    } else {
+      setFilteredInspections(allInspections)
+    }
+  }, [showInteriorOnly, allInspections])
+  
   return (
     <div className="inspections-container">
       <h2>Inspections</h2>
+      <div>
+        <button onClick={() => { setShowInteriorOnly(true) }}>Interior Only</button>
+        <button onClick={() => { setShowInteriorOnly(false)}}>Show All</button>
+      </div>
       <article className="inspections">
-        {allInspections.map((inspection) => {
+        {filteredInspections.map((inspection) => {
           return (
             <section className="inspection" key={inspection.id}>
               <header className="inspection-info">#{inspection.id}</header>
               <div>{inspection.description}</div>
+              <div>Season: {inspection.season}</div>
               <footer>
                 <div>
                   <div className="inspection-info">Interior:</div>
